096508cc7aa6579746d086975a5200a98fda7117
Json_Lee <2622336659@qq.com>
2025-05-19 23:45:08 +0800

    fix: 新增core的eslint

--- lint-staged.config.cjs
+++ lint-staged.config.cjs
@@ -1,6 +1,8 @@
 module.exports = {
-  '*.ts?(x)': ['eslint --fix'],
-  '*.vue': ['eslint --fix'],
-  '*.{scss,less,css,html}': ['stylelint --fix'],
-  '*.{ts?(x),cjs,scss,less,css,html,json,md}': ['prettier --write'],
+  "*.{js,jsx,ts,tsx}": ["eslint --fix", "prettier --write"],
+  "{!(package)*.json,*.code-snippets,.!(browserslist)*rc}": ["prettier --write--parser json"],
+  "package.json": ["prettier --write"],
+  "*.vue": ["eslint --fix", "prettier --write", "stylelint --fix"],
+  "*.{scss,less,style,html}": ["stylelint --fix", "prettier --write"],
+  "*.md": ["prettier --write"]
 };
