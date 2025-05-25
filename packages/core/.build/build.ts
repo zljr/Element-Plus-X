e1b054eb763379161f18d3b340448c1d2ec4c20d
Json_Lee <2622336659@qq.com>
2025-05-26 00:00:06 +0800

    fix: 修改一条打包信息

--- packages/core/.build/build.ts
+++ packages/core/.build/build.ts
@@ -5,28 +5,31 @@ import fg from 'fast-glob';
 const root = resolve(__dirname, '../');
 
 const entries = fg.globSync('src/components/*/*.(tsx|ts|vue)', {
-  ignore: ['src/components/**/*.d.ts', 'src/components/**/*.types.ts'],
+  ignore: ['src/components/**/*.d.ts', 'src/components/**/*.types.ts']
 });
 
 const hooksEntries = fg.globSync('src/hooks/*.(ts|tsx)', {
-  ignore: ['src/hooks/**/*.d.ts', 'src/hooks/**/*.types.ts'],
+  ignore: ['src/hooks/**/*.d.ts', 'src/hooks/**/*.types.ts']
 });
 
-const entriesObj = Object.fromEntries(entries.map((f) => {
-  return [
-    relative('src/components', f.slice(0, f.length - extname(f).length)),
-    join(root, f),
-  ];
-}));
+const entriesObj = Object.fromEntries(
+  entries.map(f => {
+    return [
+      relative('src/components', f.slice(0, f.length - extname(f).length)),
+      join(root, f)
+    ];
+  })
+);
 
-const hooksEntriesObj = Object.fromEntries(hooksEntries.map((f) => {
-  return [
-    `hooks/${relative('src/hooks', f.slice(0, f.length - extname(f).length))}`,
-    join(root, f),
-  ];
-}));
+const hooksEntriesObj = Object.fromEntries(
+  hooksEntries.map(f => {
+    return [
+      `hooks/${relative('src/hooks', f.slice(0, f.length - extname(f).length))}`,
+      join(root, f)
+    ];
+  })
+);
 
-console.log('index>>', resolve(__dirname, '../src/index.ts'));
 const buildConfig: BuildEnvironmentOptions = {
   lib: {
     name: 'ElementPlusX',
@@ -34,12 +37,12 @@ const buildConfig: BuildEnvironmentOptions = {
       index: resolve(__dirname, '../src/index.ts'),
       components: resolve(__dirname, '../src/components.ts'),
       ...entriesObj,
-      ...hooksEntriesObj,
+      ...hooksEntriesObj
     },
     fileName: (format, entryName) => {
       return `${format}/${entryName}.js`;
     },
-    formats: ['es'],
+    formats: ['es']
   },
   rollupOptions: {
     // 确保外部化处理那些你不想打包进库的依赖
@@ -47,20 +50,22 @@ const buildConfig: BuildEnvironmentOptions = {
     output: {
       // 在 UMD 构建模式下为这些外部化的依赖提供一个全局变量
       globals: {
-        vue: 'Vue',
+        vue: 'Vue'
       },
       exports: 'named', // 确保有命名导出
       assetFileNames: ((info: any) => {
         const srcName = info.originalFileNames[0];
         if (srcName) {
           if (srcName.includes('src/components/')) {
-            const fileName = srcName.replace('src/components/', '').replace('index.vue', 'index.css');
+            const fileName = srcName
+              .replace('src/components/', '')
+              .replace('index.vue', 'index.css');
             return `es/${fileName}`;
           }
         }
         return info.name;
-      }) as unknown as string,
-    },
+      }) as unknown as string
+    }
   },
   sourcemap: true,
   // 减少文件大小
@@ -68,7 +73,7 @@ const buildConfig: BuildEnvironmentOptions = {
   // CSS 处理
   cssCodeSplit: true,
   // 确保只生成一个CSS文件
-  emptyOutDir: false,
+  emptyOutDir: false
 };
 
 export default buildConfig;
